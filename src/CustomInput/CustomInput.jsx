5bae481cbe462cf7a4879ad9f97c3208147571d0
Prawesh Lamsal <prawesh.lamsal@infodevelopers.com.np>
2021-08-29 10:21:26 +0545

    updated animated height width

--- src/CustomInput/CustomInput.jsx
+++ src/CustomInput/CustomInput.jsx
@@ -2,18 +2,20 @@ import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import React, { InputHTMLAttributes, KeyboardEventHandler, useState } from 'react'
 
 const CustomInput = () => {
-    const [cusHeight, setCusHeight] = useState(1)
+    // const [cusHeight, setCusHeight] = useState(1)
+    const [cusHeight, setCusHeight] = useState("20px")
     const [cusWidth, setCusWidth] = useState("50px")
     const [value, setValue] = useState("")
 
     const inputKeyDown = (e) => {
         e.persist();
         let length = ((e.target.value.length + 4) * 10);
-        console.log(cusHeight);
+        console.log()
         if (length <= 200) {
             setCusWidth(length + "px");
+            console.log((parseInt(length / 210) + 1) * 60 + "px")
         }
-        setCusHeight(parseInt(length / 210 + "") + 1)
+        setCusHeight((parseInt(length / 200) + 1) * 15 + "px")
         if (e.key == "Enter") {
             alert("Click")
         }
@@ -26,7 +28,7 @@ const CustomInput = () => {
     return (
         <div className="custom_wrapper">
             <FormGroup className="custom_width" style={{ width: "200px" }}>
-                <Input style={{ width: cusWidth }} onKeyDown={inputKeyDown} className="custom_text_area" rows={cusHeight} type="textarea" name="email" id="exampleEmail" placeholder="Enter the value" />
+                <Input style={{ width: cusWidth, height: cusHeight }} onKeyDown={inputKeyDown} className="custom_text_area" type="textarea" name="email" id="exampleEmail" placeholder="Enter the value" />
             </FormGroup>
 
         </div>
