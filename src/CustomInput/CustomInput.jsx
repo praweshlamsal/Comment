beda6311e1e24e84b8e4f1ffd15e391a9bcafe57
Prawesh Lamsal <prawesh.lamsal@infodevelopers.com.np>
2021-08-29 11:12:42 +0545

    updated with items

--- src/CustomInput/CustomInput.jsx
+++ src/CustomInput/CustomInput.jsx
@@ -1,5 +1,6 @@
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import React, { InputHTMLAttributes, KeyboardEventHandler, useState } from 'react'
+import './customInput.css'
 // height for the expected height of the textarea to be increased,and max width for the maximum with the textarea is extended.
 const CustomInput = ({ height, maxwidth }) => {
     const [cusHeight, setCusHeight] = useState(height + "px")
@@ -23,7 +24,7 @@ const CustomInput = ({ height, maxwidth }) => {
     return (
         <div className="custom_wrapper">
             <FormGroup className="custom_width" style={{ width: (maxwidth + 5) + "px" }}>
-                <Input style={{ width: cusWidth, height: cusHeight }} onKeyDown={inputKeyDown} className="custom_text_area" type="textarea" name="email" id="exampleEmail" placeholder="Enter the value" />
+                <Input style={{ width: cusWidth, height: cusHeight }} onKeyDown={inputKeyDown} className="custom_text_area" type="textarea" name="email" id="exampleEmail" placeholder="Enter" />
             </FormGroup>
 
         </div>
